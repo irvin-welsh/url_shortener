8cf567aa2329afb544973d914097ef3b9241f5bd
irvinwelsh <irvin@vb.com>
2025-05-26 10:41:56 +0300

    added lru cache strategy

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-# url_shortener/config.py
-
 from pydantic import BaseSettings
+from functools import lru_cache
 
 class Settings(BaseSettings):
     env_name: str = "local"
     base_url: str = "http://localhost:8000"
     db_url: str= "sqlite:///./shorterner.db"
 
+@lru_cache
 def get_setting() -> Settings:
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
